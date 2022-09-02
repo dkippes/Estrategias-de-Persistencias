93c80146fb7faab2f7459739159e1613caa562b7
Diego Kippes <kippes.diego@gmail.com>
2022-09-02 01:08:31 -0300

    Update readme.mk

--- temario/unidad-2-hibernate/readme.mk
+++ temario/unidad-2-hibernate/readme.mk
@@ -1,1 +1,9 @@
-Hola
+La unidad 3 se aborda de manera práctica. El ejemplo desarrollado en clase está aquí:
+
+Ejemplo: https://github.com/EPERS-UNQ/unidad3-ejemplo-hibernate
+
+Mas material:
+
+Otro ejemplo de hibernate: https://sites.google.com/site/estrategiasdepersistencia/deprecated/hibernate
+
+Documentación oficial de hibernate - http://hibernate.org/orm/documentation/5.2/ (ver Getting Started Guide y User Guide como referencia)
